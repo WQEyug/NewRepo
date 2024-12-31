e3c8de24a5c58247a7b03305fd2da3e3175c04cb
WQEyug <241393@student.wqe.ac.uk>
2024-12-31 13:57:57 +0000

    Add files via upload

---
+++ wordgame/wordgame/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace wordgame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(236, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(281, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MintCream;
+            this.label1.Location = new System.Drawing.Point(354, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 41);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "x";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MintCream;
+            this.label2.Location = new System.Drawing.Point(470, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 41);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "x";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.MintCream;
+            this.label3.Location = new System.Drawing.Point(233, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 41);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "x";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MintCream;
+            this.label4.Location = new System.Drawing.Point(233, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 41);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "x";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.MintCream;
+            this.label5.Location = new System.Drawing.Point(354, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 41);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "x";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.MintCream;
+            this.label6.Location = new System.Drawing.Point(470, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 41);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "x";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.MintCream;
+            this.label7.Location = new System.Drawing.Point(575, 159);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 41);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "x";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.MintCream;
+            this.label8.Location = new System.Drawing.Point(141, 159);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 41);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "x";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(656, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(34, 21);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 52);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Points : 0";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "x";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ wordgame/wordgame/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+using System.Diagnostics.Tracing;
+using System.Text.RegularExpressions;
+
+namespace wordgame
+{
+    public partial class Form1 : Form
+    {
+        public string file = File.ReadAllText("ukenglish.txt");
+        public string[] lines = File.ReadAllLines("ukenglish.txt");
+        public string[] vlist = new string[2];
+        public string[] llist = new string[21];
+        public List<char> letters = new List<char> { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+        public List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+        public List<char> valid = new List<char>();
+        public int points = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static char write(ref List<char> list, ref List<char> valid)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0, list.Count);
+            char x = list[num];
+            valid.Add(x); 
+            list.Remove(list[num]);
+            return x;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Refresh";
+            string[] lines = File.ReadAllLines("ukenglish.txt");
+            valid = new List<char>();
+            textBox1.ReadOnly= false;
+            letters = new List<char> { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+            vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            label7.Text = write(ref vowels, ref valid).ToString();
+            label8.Text = write(ref vowels, ref valid).ToString();
+            label1.Text= write(ref letters, ref valid).ToString();
+            label2.Text = write(ref letters, ref valid).ToString();
+            label3.Text = write(ref letters, ref valid).ToString();
+            label4.Text = write(ref letters, ref valid).ToString();
+            label5.Text = write(ref letters, ref valid).ToString();
+            label6.Text = write(ref letters, ref valid).ToString();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (textBox1.Text == lines[i])
+                {
+                    lines[i] = null;
+                    points += textBox1.Text.Length;
+                    label9.Text = "Points : "+points;
+                }
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if (valid[0] == e.KeyChar) { e.Handled = false; }
+            if (valid[1] == e.KeyChar) { e.Handled = false; }
+            if (valid[2] == e.KeyChar) { e.Handled = false; }
+            if (valid[3] == e.KeyChar) { e.Handled = false; }
+            if (valid[4] == e.KeyChar) { e.Handled = false; }
+            if (valid[5] == e.KeyChar) { e.Handled = false; }
+            if (valid[6] == e.KeyChar) { e.Handled = false; }
+            if (valid[7] == e.KeyChar) { e.Handled = false; }
+            if(e.KeyChar=='\b') {  e.Handled = false; }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
